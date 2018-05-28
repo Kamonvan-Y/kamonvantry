1fe4236145cf811b6b38a382c60da356ccf6f469
Kamonvan Yingwong <nun.kamonvan150101@hotmail.com>
2018-05-28 14:19:25 +0700

    tryyyyyy

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      
+      <div>
+<body>
+      <div class="wrapper">
+            <nav>
+                <div class="logo">LOGO</div>
+                              <div class="wrapper"></div>
+                              <ul>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">Blog</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                    <li><a class="active" href="#">log in</a></li>
+                              </ul>
+                
+            </nav>
+           <section class="sec 1"></section>
+           <section class="content">
+             <p>somethingttttt-----</p>
+           </section>
+           </div>
+          </body>  
+        
+      </div>
+    
+    );
+  }
+}
+
+export default App;
